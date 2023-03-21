443bce34d86af6c6c4a65c10c28d203021acf925
Rob Thijssen <rthijssen@gmail.com>
2023-03-21 12:39:36 +0200

    hasBalance(babtAddress) implementation

--- handler.js
+++ handler.js
@@ -1,8 +1,29 @@
 'use strict';
 
 const hasBalance = async (babtAddress) => {
-  // todo: implement
-  return !!process.env.api_token && true;
+  const response = await fetch(
+    'https://bsc-dataseed.binance.org',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: {
+        jsonrpc: '2.0',
+        id: 1,
+        method: 'eth_call',
+        params: [
+          {
+            to: '0x2b09d47d550061f995a3b5c6f0fd58005215d7c8',
+            data: `0x70a08231000000000000000000000000${babtAddress.slice(-40)}`
+          },
+          'latest'
+        ]
+      },
+    }
+  );
+  const json = await response.json();
+  return !!json.result;
 };
 
 const hasPriorDrip = async (babtAddress, kmaAddress) => {
